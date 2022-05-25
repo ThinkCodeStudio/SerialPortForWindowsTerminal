7ec94812a2f11a21839de635c0cb058f188e8efb
think <thinkcodestudio@qq.com>
2022-05-25 20:01:14 +0800

    help&ctrlQexit

--- README.md
+++ README.md
@@ -6,10 +6,26 @@ Serial port for windows terminal, 让Windows Terminal支持串口的插件
 
 ## 使用说明
 
-![截图](https://github.com/Zhou-zhi-peng/SerialPortForWindowsTerminal/blob/main/images/001.bmp?raw=true)
-![截图](https://github.com/Zhou-zhi-peng/SerialPortForWindowsTerminal/blob/main/images/002.bmp?raw=true)
-![截图](https://github.com/Zhou-zhi-peng/SerialPortForWindowsTerminal/blob/main/images/003.bmp?raw=true)
+程序可以在终端中正常使用，`Ctrl+Q`退出程序  
+```shell
+SerialPortForWindowsTerminal.exe <串口> [其他参数]
+```
+## 参数说明
+```shell
+#程序帮助
+SerialPortForWindowsTerminal.exe COMx -h
+```
+|参数|说明|选项|默认值|
+|:--:|:--|:--:|:--:|
+|-br|波特率|无|9600|
+|-cs|数据位|无|8|
+|-sb|停止位|none,onepointfive,two|none|
+|-p|校验模式|none,add,even|none|
+|-fc|流控模式|none,software,hardware|none|
 
+## 配置说明
 
-## 配置说明：
-![使用说明](https://github.com/Zhou-zhi-peng/SerialPortForWindowsTerminal/blob/main/images/000.gif?raw=true)
+打开windows terminal进入设置 > 添加新配置文件 > 新建空配置文件 > 在命令行添加命令 > 保存  
+```shell
+<你的的程序在计算机的位置> COM1 -br 115200
+```
--- SerialForWindowsTerminal.cpp
+++ SerialForWindowsTerminal.cpp
@@ -9,6 +9,8 @@
 #include <boost/asio.hpp> 
 #include <boost/asio/windows/stream_handle.hpp>
 
+#define VERSION "1.0"
+
 #define MAX_LOADSTRING 100
 
 // 全局变量:
@@ -19,22 +21,26 @@ boost::asio::serial_port serialPort(ioctx);
 
 using PortsArray = std::vector<std::pair<std::wstring, int>>;
 
-void SetSerialConfig(int argc, const char *argv[], boost::system::error_code &ec)
+void SetSerialConfig(int argc, const char *argv[])
 {
+	std::string serialArgs[6] = { "COM1","9600","8","none","none","none" };
+	serialArgs[0] = argv[1];
 	int count = 2;
 
 	while (count < argc) {
 		if (strcmp(argv[count], "-br") == 0 || strcmp(argv[count], "--baud_rate") == 0) {
 			if (++count < argc) {
 				serialPort.set_option(boost::asio::serial_port::baud_rate(std::atoi(argv[count])), ec);
+				serialArgs[1] = argv[count];
 			}
 			else {
 				serialPort.set_option(boost::asio::serial_port::baud_rate(9600), ec);
 			}
 		}
-		else if (strcmp(argv[count], "-wl") == 0 || strcmp(argv[count], "--word_length") == 0) {
+		else if (strcmp(argv[count], "-cs") == 0 || strcmp(argv[count], "--character_size") == 0) {
 			if (++count < argc) {
 				serialPort.set_option(boost::asio::serial_port::character_size(atol(argv[count])), ec);
+				serialArgs[2] = argv[count];
 			}
 			else {
 				serialPort.set_option(boost::asio::serial_port::character_size(8), ec);
@@ -51,6 +57,7 @@ void SetSerialConfig(int argc, const char *argv[], boost::system::error_code &ec
 				else if (strcmp(argv[count], "two")) {
 					serialPort.set_option(boost::asio::serial_port::stop_bits(boost::asio::serial_port::stop_bits::two), ec);
 				}
+				serialArgs[3] = argv[count];
 			}
 			else {
 				serialPort.set_option(boost::asio::serial_port::parity(boost::asio::serial_port::parity::none), ec);
@@ -68,6 +75,7 @@ void SetSerialConfig(int argc, const char *argv[], boost::system::error_code &ec
 				else if (strcmp(argv[count], "even")) {
 					serialPort.set_option(boost::asio::serial_port::parity(boost::asio::serial_port::parity::even), ec);
 				}
+				serialArgs[4] = argv[count];
 			}
 			else {
 				serialPort.set_option(boost::asio::serial_port::parity(boost::asio::serial_port::parity::none), ec);
@@ -84,14 +92,32 @@ void SetSerialConfig(int argc, const char *argv[], boost::system::error_code &ec
 				else if (strcmp(argv[count], "hardware")) {
 					serialPort.set_option(boost::asio::serial_port::flow_control(boost::asio::serial_port::flow_control::hardware), ec);
 				}
+				serialArgs[5] = argv[count];
 			}
 			else {
 				serialPort.set_option(boost::asio::serial_port::flow_control(boost::asio::serial_port::flow_control::none), ec);
 			}
 		}
+		else if (strcmp(argv[count], "-h") == 0 || strcmp(argv[count], "--help") == 0) {
+			std::cout << "--baud_rate\t-br\t\t 波特率" << std::endl;
+			std::cout << "--character_size\t-cs\t\t 数据位" << std::endl;
+			std::cout << "--stop_bit\t-sb\t\t 停止位[none,onepointfive,two]" << std::endl;
+			std::cout << "--parity\t\t-p\t\t 检验模式[none,odd,even]" << std::endl;
+			std::cout << "--flow_control\t-fc\t\t 流控模式[none,software,hardware]" << std::endl;
+
+			return;
+		}
 		count++;
 	}
+	std::cout << "port\t\t\t" << serialArgs[0] << std::endl;
+	std::cout << "baud rate\t\t" << serialArgs[1] << std::endl;
+	std::cout << "word length\t\t" << serialArgs[2] << std::endl;
+	std::cout << "stop bit\t\t" << serialArgs[3] << std::endl;
+	std::cout << "parity\t\t\t" << serialArgs[4] << std::endl;
+	std::cout << "flow control\t\t" << serialArgs[5] << std::endl;
 }
+
+
 
 template <class TStream1, class TStream2>
 static void DoStreamToStream(TStream1 &stream1, TStream2 &stream2, std::vector<uint8_t> &buffer)
@@ -100,6 +126,18 @@ static void DoStreamToStream(TStream1 &stream1, TStream2 &stream2, std::vector<u
 		boost::asio::buffer(buffer.data(), buffer.size()),
 		[&stream1, &stream2, &buffer](const boost::system::error_code &ec, std::size_t bytes_transferred)
 		{
+			//ctrl Q 推出程序
+			if (buffer.data()[0] == 17) {
+				ioctx.stop();
+			}
+			//ctrl D 断开串口连接
+			if (buffer.data()[0] == 4) {
+
+			}
+			//ctrl R 重新连接串口
+			if (buffer.data()[0] == 18) {
+
+			}
 			if (ec)
 			{
 				std::cerr << "\033[31m" << "error : " << ec.message() << "\033[0m" << std::endl;
@@ -126,7 +164,7 @@ static void DoStreamToStream(TStream1 &stream1, TStream2 &stream2, std::vector<u
 	);
 }
 
-static boost::system::error_code DoWork(boost::asio::io_service &ioctx, boost::asio::serial_port &serialPort)
+static boost::system::error_code DoWork()
 {
 	boost::system::error_code ec;
 	boost::asio::windows::stream_handle stdinput(ioctx);
@@ -148,54 +186,74 @@ static boost::system::error_code DoWork(boost::asio::io_service &ioctx, boost::a
 
 	DoStreamToStream(serialPort, stdoutput, serialPortRecvBuffer);
 	DoStreamToStream(stdinput, serialPort, serialPortSendBuffer);
-	std::cout << "welcome use serial for windows terminal!" << std::endl;
 	ioctx.run(ec);
 	return ec;
 }
 
-int main(int argc, const char *args[])
+void ArgHelp() {
+	std::cout << "SerialForWindwosTerminal <command>" << std::endl;
+	std::cout << "help\t-h\t\t 显示帮助" << std::endl;
+	std::cout << "version\t-v\t\t 显示程序版本" << std::endl;
+	std::cout << "<port>\t\t\t\t 要连接的串口号" << std::endl;
+}
+
+int main(int argc, const char *argv[])
 {
-	hInstance = GetModuleHandle(nullptr);
-
-	DWORD consoleMode = 0;
-	auto conin = GetStdHandle(STD_INPUT_HANDLE);
-	auto conout = GetStdHandle(STD_OUTPUT_HANDLE);
-	GetConsoleMode(conin, &consoleMode);
-	consoleMode |= ENABLE_MOUSE_INPUT;
-	consoleMode &= ~ENABLE_ECHO_INPUT;
-	consoleMode &= ~ENABLE_PROCESSED_INPUT;
-	consoleMode &= ~ENABLE_LINE_INPUT;
-	consoleMode |= ENABLE_QUICK_EDIT_MODE;
-	consoleMode |= ENABLE_WINDOW_INPUT;
-	consoleMode |= ENABLE_VIRTUAL_TERMINAL_INPUT;
-	SetConsoleMode(conin, consoleMode);
-
-	GetConsoleMode(conout, &consoleMode);
-	SetConsoleMode(conout, consoleMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING | ENABLE_PROCESSED_OUTPUT);
-
-	auto hWndParent = ::GetForegroundWindow();
-	if (hWndParent == nullptr)
-		hWndParent = GetConsoleWindow();
 	if (argc > 1)
 	{
-		if (serialPort.open(args[1], ec))
-		{
-			std::cerr << "\033[31m" << "can not open " << args[1] << "\033[0m" << std::endl;
-			std::cerr << "\033[31m" << "error : " << ec.message() << "\033[0m" << std::endl;
+		if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "help") == 0) {
+			ArgHelp();
 		}
-		SetSerialConfig(argc, args, ec);
+		else if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "version") == 0) {
+			std::cout << VERSION << std::endl;
+		}
+		else {
+
+			hInstance = GetModuleHandle(nullptr);
+
+			std::cout << "welcome use serial for windows terminal!" << std::endl;
+
+			DWORD consoleMode = 0;
+			auto conin = GetStdHandle(STD_INPUT_HANDLE);
+			auto conout = GetStdHandle(STD_OUTPUT_HANDLE);
+			GetConsoleMode(conin, &consoleMode);
+			consoleMode |= ENABLE_MOUSE_INPUT;
+			consoleMode &= ~ENABLE_ECHO_INPUT;
+			consoleMode &= ~ENABLE_PROCESSED_INPUT;
+			consoleMode &= ~ENABLE_LINE_INPUT;
+			consoleMode |= ENABLE_QUICK_EDIT_MODE;
+			consoleMode |= ENABLE_WINDOW_INPUT;
+			consoleMode |= ENABLE_VIRTUAL_TERMINAL_INPUT;
+			SetConsoleMode(conin, consoleMode);
+
+			GetConsoleMode(conout, &consoleMode);
+			SetConsoleMode(conout, consoleMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING | ENABLE_PROCESSED_OUTPUT);
+
+			auto hWndParent = ::GetForegroundWindow();
+			if (hWndParent == nullptr)
+				hWndParent = GetConsoleWindow();
+
+			if (serialPort.open(argv[1], ec))
+			{
+				std::cerr << "\033[31m" << "can not open " << argv[1] << "\033[0m" << std::endl;
+				std::cerr << "\033[31m" << "error : " << ec.message() << "\033[0m" << std::endl;
+			}
+			SetSerialConfig(argc, argv);
+
+			//串口shell交互功能
+			ec = DoWork();
+			if (ec)
+			{
+				std::cerr << "\033[31m" << "error : " << ec.message() << "\033[0m" << std::endl;
+				return ec.value();
+			}
+		}
 	}
 	else
 	{
-		return ERROR_CANCELLED;
+		ArgHelp();
 	}
 
-	//串口shell交互功能
-	ec = DoWork(ioctx, serialPort);
-	if (ec)
-	{
-		std::cerr << "\033[31m" << "error : " << ec.message() << "\033[0m" << std::endl;
-		return ec.value();
-	}
+
 	return ERROR_SUCCESS;
 }
